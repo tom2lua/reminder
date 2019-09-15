b972671ca072f1df926e4714793aa6a6bf3fccc9
Nguyen Tran <tomtuyennga@gmail.com>
2019-09-15 19:36:17 +0300

    Initial Push

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <ReminderLayout msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import ReminderLayout from './components/reminder/ReminderLayout'
+
+export default {
+  name: 'app',
+  components: {
+    ReminderLayout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
